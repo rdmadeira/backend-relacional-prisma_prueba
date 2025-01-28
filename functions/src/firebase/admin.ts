b57affedee239de3d15f43d9b8190b860c62c1a8
rdmadeira <rdmadeira2@gmail.com>
2025-01-27 21:37:19 -0300

    change gts dependencies & admin from config

--- functions/src/firebase/admin.ts
+++ functions/src/firebase/admin.ts
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 
 // import serviceAccount from "./config.ts";
 //import {appendFile} from "fs";
-import serviceAccount from "./privateKey.json" with { type: "json" };
+import serviceAccount from "./config.js";
 
 const admin2 = admin.initializeApp({
   credential: admin.credential.cert(JSON.stringify(serviceAccount)),
